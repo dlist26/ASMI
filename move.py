3d00745410b25abc8adbc1c225b59ce176f2177f
dlist26 <152134268+dlist26@users.noreply.github.com>
2024-05-26 19:43:06 -0400

    Update move.py

--- move.py
+++ move.py
@@ -3,7 +3,7 @@ This is a script which moves the cnc to the location of inputted wells. This is 
 is set up and connected properly
 
 
-Part of this code are adapted from the python_to_GRBL github repository found here:
+Parts of this code are adapted from the python_to_GRBL github repository found here:
 
 https://github.com/Sam-Freitas/python_to_GRBL/tree/main
 Copyright (c) 2016 Florent Gallaire <fgallaire@gmail.com>
