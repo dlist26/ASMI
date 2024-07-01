f919cc872aa5824135cf46c3edf8dbf0a6e27f95
dlist26 <152134268+dlist26@users.noreply.github.com>
2024-07-01 16:08:57 -0400

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -241,11 +241,11 @@ def adjust_depth(run_array, d0): #using curve fit, adjust depth so that at zero 
     return run_array
 
 
-def find_E(A): #determine elastic modulus from curve fit
+def find_E(A, p_ratio): #determine elastic modulus from curve fit
     r_sphere = 0.0025
     sphere_p_ratio = 0.28
     sphere_E = 1.8 * pow(10, 11)
-    polymer_p_ratio = 0.5
+    polymer_p_ratio = p_ratio
     actual_A = A * pow(1000, 1.5)
     E_star = (actual_A * 0.75)/pow(r_sphere, 0.5)
     E_inv = 1/(E_star * (1 - pow(polymer_p_ratio, 2))) - (1 - pow(sphere_p_ratio, 2))/(sphere_E * (1 - pow(polymer_p_ratio, 2)))
@@ -375,7 +375,7 @@ else:
                 print("Fit quality may be poor")
                 break
 
-    E = find_E(fit_A)
+    E = find_E(fit_A, p_ratio)
     #print(E)
     E = adjust_E(E)
     E = round(E)
@@ -386,7 +386,7 @@ else:
             f"The range the measurement was made with was {round(min(depth_in_range), 2)} mm to {round(max(depth_in_range), 2)} mm")
     err = np.sqrt(np.diag(covariance))
     #print(covariance[0][0])
-    std_dev = round(find_E(err[0]))
+    std_dev = round(find_E(err[0], p_ratio))
     ##print(std_dev)
     print(f"Well {well}: E = {E} N/m^2, Uncertainty = {std_dev} N/m^2")
     pyplot.scatter(depth_in_range, adjusted_forces)
